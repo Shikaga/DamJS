97881108a8874852fe429bbe15ac912cbcb15a23
Shikaga <shikaga@gmail.com>
2015-06-15 15:31:52 +0100

    Perms can now be changed by checkbox

--- PermPermissionListElement.js
+++ PermPermissionListElement.js
@@ -1,16 +1,18 @@
 define(['lib/react'], function(React) {
 	return React.createClass({
     getInitialState: function() {
-      return {value: this.props.value}
+      return {value: this.props.value == "ALLOW"}
     },
     onInputChange: function(e){
-			this.props.permissions[this.props.permission] = e.target.value;
-			this.setState({value:e.target.value});
+			var allowString = e.target.checked ? "ALLOW" : "DENY";
+			this.props.permissions[this.props.permission] = allowString;
+			this.setState({value:e.target.checked});
 		},
 		render: function() {
 			return React.DOM.div({},
         React.DOM.div({},this.props.permission),
-        React.DOM.input({onChange: this.onInputChange, value: this.state.value})
+        React.DOM.span({}, "Allow: "),
+				React.DOM.input({onChange: this.onInputChange, checked: this.state.value, type: "checkbox"})
       );
 		}
 	});
