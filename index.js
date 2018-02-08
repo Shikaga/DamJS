eba4e5b6cd7717a5a51ab6e78fce144f622e62f0
Jon Paul <shikaga@gmail.com>
2018-02-08 20:17:46 +0000

    There are now more usefule default subjects

--- index.js
+++ index.js
@@ -5,20 +5,20 @@
 		var s = d.createElement('script');
 		s.src = damJSDomain + '/lib/require.js';
 
-        if (typeof(exports) !== "undefined") {
-            exportsBak = exports;
-        }
-				exports = undefined
+		if (typeof(exports) !== "undefined") {
+			exportsBak = exports;
+		}
+		exports = undefined
 
-				if (typeof(define) !== "undefined") {
-					defineBak = define;
-        }
-				define = undefined
+		if (typeof(define) !== "undefined") {
+			defineBak = define;
+		}
+		define = undefined
 
-				if (typeof(require) !== "undefined") {
-					requireBak = require;
-        }
-				require = undefined
+		if (typeof(require) !== "undefined") {
+			requireBak = require;
+		}
+		require = undefined
 
 
 		s.onload = function() {
@@ -43,19 +43,22 @@
 				}
 
 				var damJS = new DamJS(module.exports);
+				damJS.addNewMatcher("/");
 				damJS.addNewMatcher("/FX/EURUSD");
 				damJS.addNewMatcher("/FX/GBPUSD");
-                damJS.addNewMatcher("/FX/USDJPY");
-                damJS.addNewMatcher("/PRIVATE/FX");
+				damJS.addNewMatcher("/FX/USDJPY");
+				damJS.addNewMatcher("/PRIVATE");
+				damJS.addNewMatcher("/PRIVATE/FX");
 				damJS.addNewMatcher("/PRIVATE/TRADE/FX");
+				damJS.addNewMatcher("/PRIVATE/TRADE/MM");
 
 				var newElement = document.createElement('div');
 				document.body.appendChild(newElement);
 				React.renderComponent(DamJSElement({damJS: damJS}), newElement);
 
-                exports = exportsBak;
-                define = defineBak;
-                require = requireBak;
+				exports = exportsBak;
+				define = defineBak;
+				require = requireBak;
 
 			});
 		}
@@ -77,87 +80,87 @@ var _oldZIndex = 0;         // we temporarily increase the z-index during drag
 
 function OnMouseDown(e)
 {
-    // IE is retarded and doesn't pass the event object
-    if (e == null)
-        e = window.event;
+	// IE is retarded and doesn't pass the event object
+	if (e == null)
+		e = window.event;
 
-    // IE uses srcElement, others use target
-    var target = e.target != null ? e.target : e.srcElement;
+	// IE uses srcElement, others use target
+	var target = e.target != null ? e.target : e.srcElement;
 
-    // for IE, left click == 1
-    // for Firefox, left click == 0
-    if ((e.button == 1 && window.event != null ||
-        e.button == 0) &&
-        target.className == 'drag')
-    {
-        // grab the mouse position
-        _startX = e.clientX;
-        _startY = e.clientY;
+	// for IE, left click == 1
+	// for Firefox, left click == 0
+	if ((e.button == 1 && window.event != null ||
+		e.button == 0) &&
+		target.className == 'drag')
+	{
+		// grab the mouse position
+		_startX = e.clientX;
+		_startY = e.clientY;
 
-        // grab the clicked element's position
-        _offsetX = ExtractNumber(target.style.left);
-        _offsetY = ExtractNumber(target.style.top);
+		// grab the clicked element's position
+		_offsetX = ExtractNumber(target.style.left);
+		_offsetY = ExtractNumber(target.style.top);
 
-        // bring the clicked element to the front while it is being dragged
-        _oldZIndex = target.style.zIndex;
-        target.style.zIndex = 10000;
+		// bring the clicked element to the front while it is being dragged
+		_oldZIndex = target.style.zIndex;
+		target.style.zIndex = 10000;
 
-        // we need to access the element in OnMouseMove
-        _dragElement = target;
+		// we need to access the element in OnMouseMove
+		_dragElement = target;
 
-        // tell our code to start moving the element with the mouse
-        document.onmousemove = OnMouseMove;
+		// tell our code to start moving the element with the mouse
+		document.onmousemove = OnMouseMove;
 
-        // cancel out any text selections
-        document.body.focus();
+		// cancel out any text selections
+		document.body.focus();
 
-        // prevent text selection in IE
-        document.onselectstart = function () { return false; };
-        // prevent IE from trying to drag an image
-        target.ondragstart = function() { return false; };
+		// prevent text selection in IE
+		document.onselectstart = function () { return false; };
+		// prevent IE from trying to drag an image
+		target.ondragstart = function() { return false; };
 
-        // prevent text selection (except IE)
-        return false;
-    }
+		// prevent text selection (except IE)
+		return false;
+	}
 }
 
 function OnMouseMove(e)
 {
-    if (e == null)
-        var e = window.event;
+	if (e == null)
+		var e = window.event;
 
-    // this is the actual "drag code"
-    _dragElement.style.left = (_offsetX + e.clientX - _startX) + 'px';
-    _dragElement.style.top = (_offsetY + e.clientY - _startY) + 'px';
+	// this is the actual "drag code"
+	_dragElement.style.left = (_offsetX + e.clientX - _startX) + 'px';
+	_dragElement.style.top = (_offsetY + e.clientY - _startY) + 'px';
 }
 
 function OnMouseUp(e)
 {
-    if (_dragElement != null)
-    {
-        _dragElement.style.zIndex = _oldZIndex;
+	if (_dragElement != null)
+	{
+		_dragElement.style.zIndex = _oldZIndex;
 
-        // we're done with these events until the next OnMouseDown
-        document.onmousemove = null;
-        document.onselectstart = null;
-        _dragElement.ondragstart = null;
+		// we're done with these events until the next OnMouseDown
+		document.onmousemove = null;
+		document.onselectstart = null;
+		_dragElement.ondragstart = null;
 
-        // this is how we know we're not dragging
-        _dragElement = null;
-    }
+		// this is how we know we're not dragging
+		_dragElement = null;
+	}
 }
 
 function ExtractNumber(value)
 {
-    var n = parseInt(value);
+	var n = parseInt(value);
 
-    return n == null || isNaN(n) ? 0 : n;
+	return n == null || isNaN(n) ? 0 : n;
 }
 
 function InitDragDrop()
 {
-    document.onmousedown = OnMouseDown;
-    document.onmouseup = OnMouseUp;
+	document.onmousedown = OnMouseDown;
+	document.onmouseup = OnMouseUp;
 }
 
 InitDragDrop();
